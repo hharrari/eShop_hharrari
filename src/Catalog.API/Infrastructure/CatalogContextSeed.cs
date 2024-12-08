fbc54afff2193cdbc8259961c6d9bb421b0dd644
hharrari <148915499+hharrari@users.noreply.github.com>
2024-12-08 00:57:49 -0600

    Implemented Gender (25 story points).

--- src/Catalog.API/Infrastructure/CatalogContextSeed.cs
+++ src/Catalog.API/Infrastructure/CatalogContextSeed.cs
@@ -77,6 +77,7 @@ public partial class CatalogContextSeed(
         public string Type { get; set; }
         public string Brand { get; set; }
         public string Name { get; set; }
+        public string Gender { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
     }
